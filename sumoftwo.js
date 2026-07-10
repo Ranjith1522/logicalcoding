b5d8dba23a718a3ad6b2c0d4dd106a11be0311b5
unknown <ranjithkankati2@gmail.com>
2026-07-10 07:19:37 +0530

    finished in 1 go

--- sumoftwo.js
+++ sumoftwo.js
@@ -1,12 +1,21 @@
 function sumofTwo(arr,tar) {
-
+    let newArray = [];
     let tempArray = arr;
-    let ptarget = 9;
+    let ptarget = tar;
     for(let i=0;i<tempArray.length;i++) {
-        console.log(i);
+        for(let j=0;j<tempArray.length;j++) {
+            if(tempArray[i]+tempArray[j] === ptarget) {
+                console.log(tempArray[i],tempArray[j]);
+                newArray.push(i);
+                newArray.push(j);
+                return newArray;
+            }
+        }
     }
+
+    
 }
 
 const array = [2,4,8,6,9];
 const target = 14
-sumofTwo(array, target);
+const result = sumofTwo(array, target);
