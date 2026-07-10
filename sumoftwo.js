51219c1cec74b343ca29d3b48db9f20ad1af6583
unknown <ranjithkankati2@gmail.com>
2026-07-10 07:08:53 +0530

    js code

---
+++ sumoftwo.js
@@ -0,0 +1,12 @@
+function sumofTwo(arr,tar) {
+
+    let tempArray = arr;
+    let ptarget = 9;
+    for(let i=0;i<tempArray.length;i++) {
+        console.log(i);
+    }
+}
+
+const array = [2,4,8,6,9];
+const target = 14
+sumofTwo(array, target);
